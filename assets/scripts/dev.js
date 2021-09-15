25668633ea7d0070d76ca1009cb7d0624ca159c7
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-09-14 19:10:23 -0500

    started the dev script file

---
+++ assets/scripts/dev.js
@@ -0,0 +1,10 @@
+import TechnicalQuestions from "./technical.json"
+import BehavioralQuestions from "./behavioral.json"
+import AlgorithmQuestions from "./algorithm.json"
+
+function init() {
+    console.log(TechnicalQuestions)
+    let table = document.querySelector(".questionDisplay")
+}
+
+init()
