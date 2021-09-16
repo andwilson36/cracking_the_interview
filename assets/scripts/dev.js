9692b0f54f5911e0215f6009ca5e915cbe753c43
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-09-16 16:53:18 -0500

    added fetches for json file

--- assets/scripts/dev.js
+++ assets/scripts/dev.js
@@ -1,10 +1,26 @@
-import("technical.json", { assert: { type: "json" } });
-
 function init() {
-
-    console.log(data)
-
+    getAllMissions()
     let table = document.querySelector(".questionDisplay")
+}
+
+async function getAllMissions() {
+    await fetch("../assets/scripts/technical.json")
+    .then(response => {
+       return response.json();
+    })
+    .then(data => console.log(data));
+
+    await fetch("../assets/scripts/behavioral.json")
+    .then(response => {
+       return response.json();
+    })
+    .then(data => console.log(data));
+
+    await fetch("../assets/scripts/algorithm.json")
+    .then(response => {
+       return response.json();
+    })
+    .then(data => console.log(data));
 }
 
 init()
