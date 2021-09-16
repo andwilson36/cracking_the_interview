358fd3d25bd50c9f7429491c9135bebec832b958
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-09-16 17:46:59 -0500

    added dev

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -6,6 +6,11 @@ router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, '/../public/index.html'));
 });
 
+// GET /dev
+router.get("/dev", (req, res) => {
+  res.sendFile(path.join(__dirname, '/../public/views/dev.html'));
+});
+
 // GET /technical questions
 router.get("/technical-questions", (req, res) => {
   res.sendFile(path.join(__dirname, '/../public/views/technical-questions.html'));
