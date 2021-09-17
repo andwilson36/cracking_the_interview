55757a7e9fd04d4de1024570f88654711880170b
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-09-17 07:08:00 -0500

    resolve error codes

--- public/assets/scripts/dev.js
+++ public/assets/scripts/dev.js
@@ -3,16 +3,21 @@ let questions = {}
 function init() {
    getAllMissions()
    let table = document.querySelector(".questionDisplay")
+   document.querySelector("#questionForm").addEventListener('submit', newQuestion)
 }
 
 async function getAllMissions() {
-   await fetch("/api/")
-      .then(response => {
-         return response.json();
-      })
-      .then(data => questions = data);
+   // await fetch("/api/")
+   //    .then(response => {
+   //       return response.json();
+   //    })
+   //    .then(data => questions = data);
 
-   console.log(questions)
+   // console.log(questions)
+}
+
+function newQuestion(e) {
+   e.preventDefault()
 }
 
 init()
