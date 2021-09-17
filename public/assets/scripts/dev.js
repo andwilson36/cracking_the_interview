6662f1bf3b8ff1e8ba189119dc7f7901b357493f
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-09-17 09:45:31 -0500

    dynamic table from server json

--- public/assets/scripts/dev.js
+++ public/assets/scripts/dev.js
@@ -1,31 +1,54 @@
-let questions = {}
 
 function init() {
-   getAllMissions()
-   let table = document.querySelector(".questionDisplay")
-   document.querySelector("#questionForm").addEventListener('submit', newQuestion)
+	getAllMissions()
+	document.querySelector("#questionForm").addEventListener('submit', newQuestion)
 }
 
 async function getAllMissions() {
-   // await fetch("/api/")
-   //    .then(response => {
-   //       return response.json();
-   //    })
-   //    .then(data => questions = data);
+	let questions = {}
 
-   // console.log(questions)
+	await $.get("/api/technical")
+		.done((data) => {
+			questions = data
+			console.log(data);
+		})
+		.fail(() => {
+			alert("error");
+		})
+
+	for (const language in questions) {
+		for (const difficulty in questions[language]) {
+			questions[language][difficulty].forEach(element => {
+				console.log(element)
+				let table = document.querySelector("#questionDisplay").firstElementChild
+				let tableRow = document.createElement('tr')
+				let tableData = document.createElement('td')
+
+				tableRow.innerHTML =
+					`
+					<td>${'technical'}</td>
+					<td>${language}</td>
+					<td>${difficulty}</td>
+					<td>${element.question}</td>
+					<td>${element.answer}</td>
+					`
+
+				table.appendChild(tableRow)
+			});
+		}
+	}
 }
 
 function newQuestion(e) {
-   e.preventDefault()
+	e.preventDefault()
 
-   var jqxhr = $.post( "/api/technical")
-      .done((data) => {
-        console.log(data);
-      })
-      .fail(() => {
-        alert( "error" );
-      })
+	$.post("/api/technical")
+		.done((data) => {
+			console.log(data);
+		})
+		.fail(() => {
+			alert("error");
+		})
 }
 
 init()
--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,14 @@ const router = require("express").Router();
 const fs = require("fs")
 
 // GET /api/technical
-router.post("/technical", async (req, res) => {
+router.get("/technical", async (req, res) => {
 	try {
 		let questions
 		fs.readFile(__dirname + "/../private/technical.json", (err, data) => {
 			if (err) throw err;
-			res.send(JSON.parse(data))
+			questions = JSON.parse(data);
+			res.send(questions)
+			console.log(questions)
 		});
 	}
 	catch(err) {
