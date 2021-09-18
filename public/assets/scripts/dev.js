0b27d86e65e23d06a3e7c1521a0b083ff9e034b8
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-09-18 16:00:20 -0500

    revised client side to be easier to use when scaled

--- public/assets/scripts/dev.js
+++ public/assets/scripts/dev.js
@@ -1,27 +1,44 @@
 
 function init() {
-	getAllMissions()
+	getAllQuestions()
 	document.querySelector("#questionForm").addEventListener('submit', newQuestion)
+	document.querySelector("#addButton").addEventListener('click', toggleAdd)
 }
 
-async function getAllMissions() {
-	let questions = {}
+function toggleAdd() {
+	if (document.querySelector("#questionForm").style.display === 'flex') {
+		document.querySelector("#questionForm").style.display = 'none'
+		document.querySelector("#questionDisplay").style.display = 'flex'
+	} else {
+		document.querySelector("#questionForm").style.display = 'flex'
+		document.querySelector("#questionDisplay").style.display = 'none'
+	}
+}
 
+async function getAllQuestions() {
 	await $.get("/api/technical")
 		.done((data) => {
-			questions = data
-			console.log(data);
+			createTable(data)
 		})
 		.fail(() => {
 			alert("error");
+			return;
 		})
+}
 
+function clearOutTable() {
+	let tempEl = document.querySelector("#questionDisplay").firstElementChild.firstElementChild;
+	document.querySelector("#questionDisplay").firstElementChild.innerHTML = '';
+	document.querySelector("#questionDisplay").firstElementChild.appendChild(tempEl);
+}
+
+function createTable(questions) {
+	clearOutTable();
 	for (const language in questions) {
 		for (const difficulty in questions[language]) {
 			questions[language][difficulty].forEach(element => {
 				let table = document.querySelector("#questionDisplay").firstElementChild
 				let tableRow = document.createElement('tr')
-				let tableData = document.createElement('td')
 
 				tableRow.innerHTML =
 					`
@@ -40,6 +57,12 @@ async function getAllMissions() {
 
 function newQuestion(e) {
 	e.preventDefault()
+	toggleAdd()
+
+	if (document.querySelector("#question").value === '' || document.querySelector("#answer").value === '') {
+		console.error('cannot have empty fields')
+		return
+	}
 
 	$.post("/api/technical", {
 		body: JSON.stringify({
@@ -47,15 +70,16 @@ function newQuestion(e) {
 			language: document.querySelector("#questionLanguage").value,
 			difficulty: document.querySelector("#questionDifficulty").value,
 			question: document.querySelector("#question").value,
-			answer: document.querySelector("#questionType").value
+			answer: document.querySelector("#answer").value
 		})
 	}
 	)
 		.done((data) => {
-			console.log(data);
+			console.log(data)
+			createTable(data)
 		})
-		.fail(() => {
-			alert("error");
+		.fail((err) => {
+			alert(err);
 		})
 }
 
