8a3709be8878a42f0a4443c784f0b14218aca9cb
Andrew Wilson <wilsonandrew57@gmail.com>
2021-09-21 18:45:16 -0500

    js for settings button and cancel button

--- public/assets/scripts/technical.js
+++ public/assets/scripts/technical.js
@@ -1,3 +1,26 @@
-import Questions from "./technical.json"
+//import Questions from "./technical.json"
 
-console.log(Questions.javascript.basics)
+// console.log(Questions.javascript.basics)
+
+function init() {
+    hideModel()
+}
+
+function hideModel() {
+    document.querySelector(".qa-settings-modal")
+        .style.display = "none"; 
+}
+
+document
+    .querySelector(".settings-btn")
+    .addEventListener("click", function(event) {
+        document.querySelector(".qa-settings-modal")
+            .style.display = "block";
+        event.preventDefault();
+    });
+
+document
+    .querySelector(".cancel-button")
+    .addEventListener("click", hideModel);
+    
+init();
