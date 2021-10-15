7dcb82923b69e4618c30c5bba54f136b08d10a44
Andrew Wilson <wilsonandrew57@gmail.com>
2021-10-14 22:11:38 -0500

    prints question to screen and checks for repeat

--- public/assets/scripts/behavioral.js
+++ public/assets/scripts/behavioral.js
@@ -18,5 +18,24 @@ async function getAllQuestions() {
 
 function ranQuestion() {
     let randomQuestion = a[0].behavioral.questions[Math.floor(Math.random() * a[0].behavioral.questions.length)];
-    console.log(randomQuestion)
+    checkForRepeat(randomQuestion) ? ranQuestion() : serveQuestion(randomQuestion);
+}
+
+function checkForRepeat(question) {
+    let prev = localStorage.getItem("prevQ");
+    if (prev === question.question) {
+        return true;
+    } else {
+        localStorage.setItem("prevQ", question.question);
+        return false;
+    }
+}
+
+function serveQuestion(question) {
+    document
+        .querySelector(".content-question")
+        .textContent = question.question;
+    document
+        .querySelector(".answer")
+        .textContent = question.answer;
 }
